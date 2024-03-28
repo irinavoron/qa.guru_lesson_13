072d695827754654b61fd0aa352a19faf78aff52
Irina.Voron <yarutich.irina@gmail.com>
2024-03-28 14:27:06 +0300

    Initial commit

---
+++ demoqa_tests/application.py
@@ -0,0 +1,13 @@
+from demoqa_tests.pages.left_panel import LeftPanel
+from demoqa_tests.pages.registration_form import RegistrationForm
+from demoqa_tests.pages.simple_registration_form import SimpleRegistrationForm
+
+
+class ApplicationManager:
+    def __init__(self):
+        self.simple_registration = SimpleRegistrationForm()
+        self.left_panel = LeftPanel()
+        self.registration = RegistrationForm()
+
+
+app = ApplicationManager()
---
+++ tests/test_simple_registration_form.py
@@ -0,0 +1,23 @@
+from selene import have
+
+from demoqa_tests.application import app
+from demoqa_tests.data import users
+
+
+def test_simple_registration_form():
+    user = users.admin
+
+    app.left_panel.open_simple_registration_form()
+    app.simple_registration.fill_full_name(f'{user.first_name} {user.last_name}')
+    app.simple_registration.fill_email(user.email)
+    app.simple_registration.fill_current_address(user.current_address)
+    app.simple_registration.fill_permanent_address(user.permanent_address)
+    app.simple_registration.submit_form()
+
+    app.simple_registration.output.should(have.texts(
+        f'{user.first_name} {user.last_name}',
+        user.email,
+        user.current_address,
+        user.permanent_address
+    )
+    )
